a7d383585bf66d530ddd6ae36035dc6f5c8b42d1
Camilla Harris <camilla.harris@ucl.ac.uk>
2024-07-31 10:25:34 +0100

    versions

--- terraform/modules/jupyter/versions.tf
+++ terraform/modules/jupyter/versions.tf
@@ -1,12 +1,12 @@
-# Enforce minimum Terraform and provider version numbers.
 terraform {
-  required_providers {
 
+  required_version = "1.8.5"
+
+  required_providers {
     harvester = {
       source  = "harvester/harvester"
       version = "0.6.4"
     }
   }
 
-  required_version = ">= 1.8.5"
 }
