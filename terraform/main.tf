4a230706f1ec50ffbd73fff9cf3ed97d2d2e6c79
Camilla Harris <camilla.harris@ucl.ac.uk>
2024-09-27 17:09:37 +0100

    instance module for storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,17 @@ module "ssrc-jupyter-dept" {
 
   public_key_openssh = var.public_key_openssh
 }
+
+module "online-storage-dept" {
+  source = "./modules/online-storage"
+
+  namespace = "ssrc-ns"
+  vm_prefix = "ssrc-nfs-dept"
+  image_id  = data.harvester_image.rhel9.id
+
+  disk_settings = {
+
+  }
+
+  public_key_openssh = var.public_key_openssh
+}
