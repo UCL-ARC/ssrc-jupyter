ffbd942a7b7f833a5ddd6a127e4ca91ba0ce6dc3
Camilla Harris <camilla.harris@ucl.ac.uk>
2024-09-30 14:54:16 +0100

    get the ip address for the server

--- terraform/modules/jupyter/output.tf
+++ terraform/modules/jupyter/output.tf
@@ -3,3 +3,8 @@ output "access_url" {
   value       = local.access_url
   description = "The URL from which Jupyter can be accessed"
 }
+
+output "ip_address" {
+  value       = resource.harvester_virtualmachine.vm.network_interface[0].ip_address
+  description = "The server's IP address"
+}
---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+output "ip_address" {
+  value       = module.jupyter.ip_address
+  description = "The JupyterHub server's IP address"
+}
