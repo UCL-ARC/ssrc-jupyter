cd7f434637b6c2681de9c1642d7585a05181c7c6
Camilla Harris <31481972+cdkharris@users.noreply.github.com>
2024-10-18 15:53:54 +0100

    outputs for ip and url

--- terraform/modules/jupyter/output.tf
+++ terraform/modules/jupyter/output.tf
@@ -1,5 +1,10 @@
 # List your outputs here.
-output "access_url" {
+output "url" {
   value       = local.access_url
-  description = "The URL from which Jupyter can be accessed"
+  description = "The URL from which JupyterHub server can be accessed"
+}
+
+output "ip_addr" {
+  value       = resource.harvester_virtualmachine.vm[*].network_interface[0].ip_address
+  description = "The IP address from which JupyterHub server can be administered"
 }
