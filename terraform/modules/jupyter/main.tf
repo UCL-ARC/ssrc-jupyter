576911cdc2707e10879278d4652f4354cb74ef52
Camilla Harris <camilla.harris@ucl.ac.uk>
2024-10-23 16:34:45 +0100

    fix variable name

--- terraform/modules/jupyter/main.tf
+++ terraform/modules/jupyter/main.tf
@@ -29,7 +29,7 @@ resource "harvester_cloudinit_secret" "cloud-config-jupyter" {
         aad_client_secret = var.aad_client_secret
         aad_tenant_id     = var.aad_tenant_id
         image             = var.jupyter_image
-        image_tag         = var.jupyter_image_tag
+        image_tag         = var.jupyter_image_version
       }
       )
     )
